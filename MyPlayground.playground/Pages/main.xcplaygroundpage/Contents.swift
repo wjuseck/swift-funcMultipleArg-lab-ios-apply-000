26509d04777c4d9345fd8e5582a66d7df26553f4
JimCampagno <jimcampagno@gmail.com>
2016-05-16 16:27:01 -0400

    add playground file

---
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -0,0 +1,45 @@
+/*: Outline
+ 
+ 
+ # Functions with multiple arguments
+ 
+ ### Readings associated with this lab
+ 
+ * [Functions](https://github.com/learn-co-curriculum/swift-functions-readme)
+ * [Functions with multiple arguments](https://github.com/learn-co-curriculum/swift-funcMultipleArg-readme)
+
+ */
+/*: question1
+ ### 1. Create a function that will take in a continent and the amount of countries in that continent. It should print the following sentence "____ is a continent which contains ___ countries".
+ */
+// write your code here
+
+
+
+
+/*: question2
+ ### 2. Another question about functions.. weeee
+ */
+// write your code here
+
+
+
+
+/*: question3
+ ### 3. Moar questions...
+ */
+// write your code here
+
+
+/*: question4
+ ### 4. Function function function
+ */
+// write your code here
+
+
+
+
+/*:
+ [Solution](solution)
+ */
+// ❤️
